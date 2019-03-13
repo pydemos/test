c79b76ba8eb261493b30a6039d768779dd9216fb
Keaween <158880449@qq.com>
2019-03-13 23:59:43 +0800

    新增

---
+++ cards_tools.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Keawen
+def show_meun():
+    print("*"*50)
+    print("欢迎使用名片系统")
+    print("")
+    print("1.新增名片")
+    print("2.显示全部")
+    print("3.搜索名片")
+    print("")
+    print("0.退出系统")
+    print("*"*50)
